f894c21fa8961b8a7b8bb65afb80f99c353c4780
sunchara <sunchara@gmail.com>
2024-07-05 19:16:06 +0300

    ENH: custom staff in utils

--- BigMatrixUtils.hpp
+++ BigMatrixUtils.hpp
@@ -16,3 +16,26 @@ bool equalsByEps(const struct SimpleBlockMatrix<BLOCK_SIZE>& lhs, const struct S
     }
     return true;
 }
+
+template<size_t BLOCK_SIZE = 512>
+struct BigSquareMatrix<BLOCK_SIZE> getToeplizMatrix(size_t n, size_t m)
+{
+    struct BigSquareMatrix<BLOCK_SIZE> mm(n, m);
+
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < m; ++j)
+            mm.at(i, j) = i + j;
+
+    return mm;
+}
+
+template<size_t BLOCK_SIZE = 512>
+struct BigSquareMatrix<BLOCK_SIZE> getIdentityMatrix(size_t n, size_t m)
+{
+    struct BigSquareMatrix<BLOCK_SIZE> mm(n, m);
+
+    for (size_t i = 0; i < std::min(n, m); ++i)
+        mm.at(i, i) = 1;
+
+    return mm;
+}
